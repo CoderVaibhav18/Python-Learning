063f2aaae6aa7e268879b6cf2db4a1cb943f8e41
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-22 20:49:28 +0530

    escape sequences in strings

---
+++ day5/escapSequenceChar.py
@@ -0,0 +1,2 @@
+# If we got this o/p, Im the good "boy" but the viewer is note a good "boy"... then use Escape Sequences character
+print('Im the good \"boy\" but the viewer is note a good \"boy\"')
