65275ac9952b5b0e75c992d72b1cf4f817cde563
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-22 20:40:19 +0530

    multiple line comment

--- day5/comment.py
+++ day5/comment.py
@@ -1,1 +1,6 @@
 # Comments in programming language that exactly not executable.
+# In case, we forget what we are doing in the program that in this case we write comments
+"""
+this is multiple line comment
+"""
+print(9)
