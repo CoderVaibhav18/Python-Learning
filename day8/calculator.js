570f60f54e81a46eb2b78d06ab0cdf547e5687e7
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-22 23:36:52 +0530

    Add calculator functionality with basic arithmetic operations

---
+++ day8/calculator.js
@@ -0,0 +1,7 @@
+n1 = 9
+n2 = 9
+print("Additon of", n1, "+", n2, "is", n1+n2)
+print("Substraction of", n1, "-", n2, "is", n1-n2)
+print("Multiplication of", n1, "*", n2, "is", n1*n2)
+print("Division of", n1, "/", n2, "is", n1/n2)
+print("Exponential of", n1, "^", n2, "is", n1**2)
