c53ab43779281294dfbbfb213cd584781b46cce6
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-22 22:06:23 +0530

    different data types in Python

---
+++ day6/variableNDataType.py
@@ -0,0 +1,25 @@
+a = 9
+b = 8.9
+str = '7'
+c = complex(8, 9)
+print(a, b, str)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(str))
+
+list = [8, 9, 18, 7, 45, [76, 8], 98]
+print(list)
+print(type(list))
+
+tuple = (("vaibhav", "sathe"), 676, 9809, 90)
+print(tuple)
+print(type(tuple))
+
+dict = {
+  "name": "vaibhav",
+  "age": 20,
+  "canVote": True
+}
+print(dict["name"])
+print(type(dict))
