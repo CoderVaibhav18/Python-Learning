24ce90906c718c99089ac730f0bfe6676f9e423c
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-22 21:00:52 +0530

    Fix escape sequences in print statement and add separator example

--- day5/escapSequenceChar.py
+++ day5/escapSequenceChar.py
@@ -1,2 +1,4 @@
 # If we got this o/p, Im the good "boy" but the viewer is note a good "boy"... then use Escape Sequences character
-print('Im the good \"boy\" but the viewer is note a good \"boy\"')
+print('I\'m the good \'boy\' \nbut the viewer is also a good \'boy\'')
+# seperator, sep="-", print after print statement ends with this paraments end="18"
+print ("hey", 6, 7, 8, sep="-", end="null")
