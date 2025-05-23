cc43721247aa460ca83ec838c027d895f8416b99
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-23 20:43:30 +0530

    Fix exponential calculation placement in calculator

--- day8/calculator.py
+++ day8/calculator.py
@@ -5,10 +5,10 @@ try:
     print("Addition of", n1, "+", n2, "is", n1 + n2)
     print("Subtraction of", n1, "-", n2, "is", n1 - n2)
     print("Multiplication of", n1, "*", n2, "is", n1 * n2)
+    print("Exponential of", n1, "^", n2, "is", n1 ** n2)
     if n2 == 0:
         print("Division of", n1, "/", n2, "is Error: Division by zero")
     else:
         print("Division of", n1, "/", n2, "is", n1 / n2)
-    print("Exponential of", n1, "^", n2, "is", n1 ** n2)
 except Exception as e:
     print("An error occurred:", e)
