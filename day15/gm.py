1edba95244d9421868367f572b7a8c3bb56c46d0
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-25 00:24:49 +0530

    Fix conditional check for afternoon greeting in time-based message

--- day15/gm.py
+++ day15/gm.py
@@ -4,7 +4,7 @@ print(timestamp)
 
 if int(time.strftime('%H')) >= 5 and int(time.strftime('%H')) < 12:
   print("Gm")
-elif int(time.strftime('%H')) >= 1 and int(time.strftime('%H')) <= 5:
+elif int(time.strftime('%H')) >= 1 and int(time.strftime('%H')) < 5:
   print("GA")
 else:
   print("GN")
