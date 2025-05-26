6f9e00b4c30ce6de485e2e584a5d761c8cb0d22d
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-26 23:31:28 +0530

    Add initial loop examples for iterating over ranges and collections

---
+++ day17/loops.py
@@ -0,0 +1,19 @@
+# name = 'vaibhav'
+
+# for nm in name:
+#   print(nm, end=", ")
+#   if nm == 'b':
+#     print("This is something special")
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+
+# for color in colors:
+#   print(color)
+#   for char in color:
+#     print(char)
+
+# for i in range(5):
+#   print(i+1)
+  
+for i in range(1, 21, 2):
+  print(i)
