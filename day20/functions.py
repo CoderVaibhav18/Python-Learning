d03e07cb0bc82533fb20de67a226a624c63c0ab6
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-27 00:22:36 +0530

    Add function to determine voting eligibility based on age

---
+++ day20/functions.py
@@ -0,0 +1,15 @@
+# def greaterNum(a, b):
+#   if a > b:
+#     print(a, "is greater")
+#   else:
+#     print(b, "is greater")
+    
+# greaterNum(991, 34)
+
+def theyCanVote(age):
+  if age >= 18:
+    print("They can vote")
+  else:
+    print("They cannot vote")
+    
+theyCanVote(19)
