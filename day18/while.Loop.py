53a6485bfa286b81017eeb95988ea67effebf50a
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-26 23:48:45 +0530

    Add while loop example for basic iteration and exit message

---
+++ day18/while.Loop.py
@@ -0,0 +1,14 @@
+i = 0
+while i <= 3:
+  print(i)
+  i += 1
+else:
+  print("while loop exited successfully")
+
+# fact = 1
+# n = 5
+# i = 1
+# while i <= n:
+#   fact *= i
+#   i = i + 1
+# print(fact)
