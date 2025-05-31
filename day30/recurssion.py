201fe3746d94ce2218284f1140d6ac70076039cb
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-05-31 16:28:38 +0530

    Add factorial function implementation with example usage

---
+++ day30/recurssion.py
@@ -0,0 +1,12 @@
+def factorial(n):
+  if(n==0 or n==1):
+    return 1
+  return n * factorial(n-1)
+
+print(factorial(5))
+
+# factorial(5) = 5 * factorial(4)
+# factorial(4) = 5 * 4 * factorial(3)
+# factorial(3) = 5 * 4 * 3 * factorial(2)
+# factorial(2) = 5 * 4 * 3 * 2 * factorial(1)
+# factorial(1) = 5 * 4 * 3 * 2 * 1 = 120
