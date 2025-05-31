433a8134e2208a224cb6098a85c2c6d05d3ae50e
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-01 00:00:23 +0530

    Add initial dictionary implementation with key-value printing examples

---
+++ day33/dictionary.py
@@ -0,0 +1,13 @@
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info) 
+print(info.keys())
+print(info.values())
+
+for key in info.keys():
+  print(f" {key} : {info[key]}")
+
+print(info.items())
+
+for key, value in info.items():
+  print(f"{key} is {value}") 
+  
