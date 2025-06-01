ac44ca9c8ad5a54b6c31041443fa9f4ad88af142
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-01 23:16:26 +0530

    Add custom error handling for user input with NameError

---
+++ day38-coustomErr/customErr.py
@@ -0,0 +1,11 @@
+# a = int(input("Enter number between 5-9:"))
+
+# if(a<5 or a>9):
+#   raise ValueError("Value should be between 5-9")
+
+a = input("Enter string: ")
+
+if(a=="quit"):
+  print("not error")
+else:
+  raise NameError("plzz enter quit")
