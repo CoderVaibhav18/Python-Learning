fac867761f1a6d4e50cdf889471ba8c1c3bdafa8
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-02 20:24:26 +0530

    Refactor loop to start enumeration at 1 for accurate index display

--- day42-enumerate-func/enumerateFunc.py
+++ day42-enumerate-func/enumerateFunc.py
@@ -7,7 +7,7 @@
 #   index += 1
 
 marks = [34, 56, 78, 98, 90, 77, 99, 56]
-for index, mark in enumerate(marks):
+for index, mark in enumerate(marks, start=1):
   print(mark)
   if index == 3:
     print(f"Congrats Vaibhav!! You score good marks i.e., {marks[index]}")
