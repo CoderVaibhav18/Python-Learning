24d2b14bce121dd1dd36ef1e9838ca7d8988af4d
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-02 20:05:13 +0530

    Refactor code to display marks and add congratulatory message for specific score

--- day42-enumerate-func/enumerateFunc.py
+++ day42-enumerate-func/enumerateFunc.py
@@ -1,4 +1,12 @@
-fruits = ["Mango", "Cheeku", "Banana", "Apple", "PineApple"]
+marks = [34, 56, 78, 98, 90, 77, 99, 56]
+index = 0
+for mark in marks:
+  print(mark)
+  if index == 3:
+    print(f"Congrats Vaibhav!! You score good marks i.e., {marks[index]}")
+  index += 1
 
-for index, fruit in enumerate(fruits):
-  print(index+1, fruit)
+# fruits = ["Mango", "Cheeku", "Banana", "Apple", "PineApple"]
+
+# for index, fruit in enumerate(fruits):
+#   print(index, fruit)
