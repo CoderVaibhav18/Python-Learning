43559b50d98eac0497f9d177e33935a2f6802c0b
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-02 19:47:39 +0530

    Add comment for user input handling in secret code translation program

---
+++ day41-short-hand-if-else/shorthandIfElse.py
@@ -0,0 +1,9 @@
+# a = 45
+# b = 45
+# print(f"{a} is bigger") if a > b else print("a & b is euqal") if a == b else print(f"{b} is bigger") 
+
+# a = 42
+# b = 45
+# c = a if a > b else "a & b is euqal" if a == b else b
+
+# print(f"Your number {c} is bigger")
