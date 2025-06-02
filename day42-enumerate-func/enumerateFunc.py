dca261e2a15fbad33d01aa7caa90ba0c6a7f4f1a
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-02 20:18:03 +0530

    Refactor loop to use enumerate for improved readability and efficiency

--- day42-enumerate-func/enumerateFunc.py
+++ day42-enumerate-func/enumerateFunc.py
@@ -1,12 +1,13 @@
+# marks = [34, 56, 78, 98, 90, 77, 99, 56]
+# index = 0
+# for mark in marks:
+#   print(mark)
+#   if index == 3:
+#     print(f"Congrats Vaibhav!! You score good marks i.e., {marks[index]}")
+#   index += 1
+
 marks = [34, 56, 78, 98, 90, 77, 99, 56]
-index = 0
-for mark in marks:
+for index, mark in enumerate(marks):
   print(mark)
   if index == 3:
     print(f"Congrats Vaibhav!! You score good marks i.e., {marks[index]}")
-  index += 1
-
-# fruits = ["Mango", "Cheeku", "Banana", "Apple", "PineApple"]
-
-# for index, fruit in enumerate(fruits):
-#   print(index, fruit)
