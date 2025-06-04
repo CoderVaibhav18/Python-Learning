aca2add38249dabc2eea7286df6dc5ba0ab09042
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-05 00:15:35 +0530

    Comment out file writing logic in readReadlines.py

--- day50-read-readlines-methods/readReadlines.py
+++ day50-read-readlines-methods/readReadlines.py
@@ -14,7 +14,7 @@
 
 #   print(line)
   
-f = open('myfile2.txt', 'w')
-lines = ['line 1\n', 'line 2\n', 'line 3\n']
-f.writelines(lines)
-f.close()
+# f = open('myfile2.txt', 'w')
+# lines = ['line 1\n', 'line 2\n', 'line 3\n']
+# f.writelines(lines)
+# f.close()
