0c7c8790882076c17af3b5b3ee8b515dc89c9d7a
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-05 22:30:16 +0530

    Refactor snakeWaterGun.py to improve game logic structure and enhance clarity

--- day55-snake-water-gun/snakeWaterGun.py
+++ day55-snake-water-gun/snakeWaterGun.py
@@ -3,3 +3,13 @@
 # player =  S  0  D W L
 #           W  1  L D W
 #           G  2  W L D
+
+import random
+
+your_choice = int(input("Enter your choice 0 for 'snake', 1 for 'water' & 2 for 'gun': "))
+comp_choice = random.randint(0, 2)
+choice = ["snake", "water", "gun"]
+
+if your_choice == comp_choice:
+  print(f"Game is tie, Bcoz your_choice: {choice[your_choice]}, comp_choice: {choice[comp_choice]}")
+elif ``:
