57a61f82194f455e98a927a7874dcbea9f024ef0
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-07 22:34:37 +0530

    Refactor Library class initialization and method names for clarity

--- day64-ex6-Library-management-system/libraryManagementSystem.py
+++ day64-ex6-Library-management-system/libraryManagementSystem.py
@@ -1,27 +1,24 @@
 # Write a Library class with no_of_books and books as two instance variables. Write a program to create a library from this Library class and show how you can print all books, add a book and get the number of books using different methods. Show that your program doesnt persist the books after the program is stopped!
 
 class Library:
-  def __init__(self, books=None):
-    if books is None:
-      self.books = [] 
-    else:
-      self.books = books
+  def __init__(self, books):
+    self.books = books
     self.no_of_books = len(self.books)
     
-  def show(self):
-    print("Books in library:", self.books)
+  def show_books(self):
+    print(f"Books in Library:", self.books)
     
   def add_books(self, book):
     self.books.append(book)
     self.no_of_books = len(self.books)
-    print(f'"{book}" added to the library.')
-    
+    print(f"{book} is add into the Library")
+  
   def get_no_of_books(self):
-    print("Number of books:", self.no_of_books)
-    
-a = Library(["Harry Potter", "vaibhav"])
-a.show()
+    print(f"Total number of books is: {self.no_of_books}")
+  
+a = Library(["Harray portter", "1965"])
+a.show_books()
 a.get_no_of_books()
-a.add_books("sathe")
-a.show()
+a.add_books("Ego")
+a.show_books()
 a.get_no_of_books()
