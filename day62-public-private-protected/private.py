d0aa05661c04ff83e32cb38768195d65c8a749c4
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-07 16:06:46 +0530

    Access Modifiers in Python | Python Tutorial - Day #62

---
+++ day62-public-private-protected/private.py
@@ -0,0 +1,14 @@
+# Private Access Modifier - By definition, Private members of a class (variables or methods) are those members which are only accessible inside the class. We cannot use private members outside of class.
+
+# Name mangling - Name mangling in Python is a technique used to protect class-private and superclass-private attributes from being accidentally overwritten by subclasses. Names of class-private and superclass-private attributes are transformed by the addition of a single leading underscore and a double leading underscore respectively.
+
+class MyClass:
+    def __init__(self):
+        self._nonmangled_attribute = "I am a nonmangled attribute"
+        self.__mangled_attribute = "I am a mangled attribute"
+
+my_object = MyClass()
+
+print(my_object._nonmangled_attribute) # Output: I am a nonmangled attribute
+# print(my_object.__mangled_attribute) # Throws an AttributeError
+print(my_object._MyClass__mangled_attribute) # Output: I am a mangled attribute
