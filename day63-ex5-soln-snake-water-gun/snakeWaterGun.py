07defca1e9dcde80b1db7d138e85dbdaf2f8a8f8
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-07 21:45:16 +0530

    Fix logic in check function for snake-water-gun game

--- day63-ex5-soln-snake-water-gun/snakeWaterGun.py
+++ day63-ex5-soln-snake-water-gun/snakeWaterGun.py
@@ -4,13 +4,7 @@ def check(comp, user):
   if comp ==user:
     return 0
     
-  if(comp == 0 and user ==1):
-    return -1
-    
-  if(comp == 1 and user ==2):
-    return -1
-    
-  if(comp == 2 and user == 0):
+  if(comp == 0 and user ==1) or (comp == 1 and user ==2)(comp == 2 and user == 0):
     return -1
 
   return 1
