8b069b6649ce5f6a9e3692cc7411d0570a7271fd
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-09 16:05:38 +0530

    Refactor superKeyword.py to include detailed explanations and examples of the super() keyword in Python

--- day72-super-keyword/superKeyword.py
+++ day72-super-keyword/superKeyword.py
@@ -1,14 +1,36 @@
-class Employee:
-  def __init__(self, name, id):
-    self.name = name
-    self.id = id
+# Super keyword in Python
+# The super() keyword in Python is used to refer to the parent class. It is especially useful when a class inherits from multiple parent classes and you want to call a method from one of the parent classes.
 
-class Programmer(Employee):
-  def __init__(self, name, id, lang):
-    super().__init__(name, id)
-    self.lang = lang
+# When a class inherits from a parent class, it can override or extend the methods defined in the parent class. However, sometimes you might want to use the parent class method in the child class. This is where the super() keyword comes in handy.
 
-vaibhav = Programmer("vaibhav", "2345", "Python")
-print(vaibhav.name)
-print(vaibhav.id)
-print(vaibhav.lang)
+# Here's an example of how to use the super() keyword in a simple inheritance scenario:
+
+class ParentClass:
+    def parent_method(self):
+        print("This is the parent method.")
+class ChildClass(ParentClass):
+    def parent_method(self):
+        print("Vaibhav") 
+        super().parent_method()
+    def child_method(self):
+        print("This is the child method.")
+        super().parent_method()
+child_object = ChildClass()
+child_object.child_method()
+child_object.parent_method()
+
+
+# class Employee:
+#   def __init__(self, name, id):
+#     self.name = name
+#     self.id = id
+
+# class Programmer(Employee):
+#   def __init__(self, name, id, lang):
+#     super().__init__(name, id)
+#     self.lang = lang
+
+# vaibhav = Programmer("vaibhav", "2345", "Python")
+# print(vaibhav.name)
+# print(vaibhav.id)
+# print(vaibhav.lang)
