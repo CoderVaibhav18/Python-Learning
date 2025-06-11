eca6cecef2e529e7537bfc1ea6a14404d98f071f
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-11 13:15:16 +0530

    Remove unused import statement for os in PDF manipulation program

--- day76-ex8/ex8.py
+++ day76-ex8/ex8.py
@@ -4,7 +4,7 @@
 # Write a program to manipulate pdf files using pyPDF. Your programs should be able to merge multiple pdf files into a single pdf. You are welcome to add more functionalities
 
 from pypdf import PdfWriter
-import os
+# import os
 
 merger = PdfWriter()
 pdf_files = []
