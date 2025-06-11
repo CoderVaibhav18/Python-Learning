8b26016ffaf5a2f1ec995565dc91273bb5b7fcaf
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-11 15:59:59 +0530

    Implement multilevel inheritance with Animal, Dog, and GoldenRetriever classes

---
+++ day80-multileve-inheritance/multilevelInheritance.py
@@ -0,0 +1,29 @@
+class Animal:
+  def __init__(self, name, species):
+    self.name = name
+    self.species = species
+    
+  def show(self):
+    print(f"Name: {self.name}")
+    print(f"Species: {self.species}")
+    
+class Dog(Animal):
+  def __init__(self, name, breed):
+    Animal.__init__(self, name, species="Dog")
+    self.breed = breed
+  
+  def show(self):
+    Animal.show(self)
+    print(f"Breed: {self.breed}")
+    
+class GoldenRetriever(Dog):
+  def __init__(self, name, color):
+    Dog.__init__(self, name, breed="Golden Retriever")
+    self.color = color
+    
+  def show(self):
+    Dog.show(self)
+    print(f"Color: {self.color}")
+    
+a = GoldenRetriever("Tommy", "black")
+a.show()
