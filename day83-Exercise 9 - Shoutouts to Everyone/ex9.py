2c9b5e9fc548157d5528c801196df136416fb414
vaibhav sathe <vaibhavsathe160@gmail.com>
2025-06-12 15:46:56 +0530

    Remove pyttsx3 implementation and retain win32com for text-to-speech functionality in shoutout script

--- day83-Exercise 9 - Shoutouts to Everyone/ex9.py
+++ day83-Exercise 9 - Shoutouts to Everyone/ex9.py
@@ -1,12 +1,3 @@
-# import pyttsx3
-
-# names = ["Vaibhav", "Raj",  "Samay"]
-# engine = pyttsx3.init()  
-
-# for name in names:
-#   engine.say(f"Shoutout to {name}")  
-#   engine.runAndWait()  
-  
 import win32com.client
 
 speaker = win32com.client.Dispatch("SAPI.SpVoice")
